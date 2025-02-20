f1228b8a3bc2cecf9313bb1f66d890fa5b5f2d49
elite <elitetuber1122@gmail.com>
2025-02-20 13:04:05 +0600

    AuthProvider Section Done

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import {
   RouterProvider
 } from "react-router-dom";
 import { router } from './routes/Router.jsx';
+import AuthProvider from './provider/AuthProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
