f9f0c7ad2042a1703ebb30cb52b6459ffd1d4a2e
elite <elitetuber1122@gmail.com>
2025-02-20 10:29:06 +0600

    Basic route setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import {
-  createBrowserRouter,
-  RouterProvider,
+  RouterProvider
 } from "react-router-dom";
+import { router } from './routes/Router.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
